c49bf86186972e3834a0b15293c67bd15c9f4842
VasylArtymovych <art777vasss@gmail.com>
2022-05-01 14:00:38 +0300

    added articles renderFn

--- src/JS/NewsApiServise.js
+++ src/JS/NewsApiServise.js
@@ -10,12 +10,14 @@ export default class NewsApiService{
                 'X-Api-Key': "8868dd2683f64984b0e4e94bab24faa3"
             }
         }
-        const url = `https://newsapi.org/v2/everything?q=${this.searchQuery}&language='en'&pageSize='5'$page=${this.page}`;
+        const url = `https://newsapi.org/v2/everything?q=${this.searchQuery}&language=en&pageSize=5&page=${this.page}`;
 
-        fetch(url, options)
+        return fetch(url, options)
         .then(response => response.json())
         .then((data) => {
             this.incrementPage();
+            
+            return data.articles;
         })
     }
 
--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 import './css/styles.css';
 import NewsApiService from './JS/NewsApiServise';
 import LoadMoreArticels from './JS/load-more-artiicels';
+import articlesTmpl from './templates/search-articles.hbs'
 
 const refs = {
     searchForm: document.querySelector('#fetch-form'),
     searchBtn: document.querySelector('.js-btn-subm'),
-    loadMoreBtn: document.querySelector('.js-load-more-btn')
+    loadMoreBtn: document.querySelector('.js-load-more-btn'),
+    articles:document.querySelector('.js-articles')
 }
 
 const newsApiServise = new NewsApiService();
 const loadMoreArticels = new LoadMoreArticels({selector: '[data-action="load-more"]'})
 
-refs.loadMoreBtn.addEventListener('click', onSearchMoreBtn)
+refs.loadMoreBtn.addEventListener('click', onLoadhMoreBtn)
 refs.searchForm.addEventListener('submit', onSubmit);
 
 function onSubmit(evt){
@@ -21,11 +23,25 @@ function onSubmit(evt){
     if(search === '') return alert('fill the search field!');
 
     newsApiServise.query = search;
+    clearArticlesContainer()
     newsApiServise.resetPage()
-    newsApiServise.fetchArticels()
+    newsApiServise.fetchArticels().then((articles)=> {
+        clearArticlesContainer()
+        renderArticles(articles);
+    })
+
     evt.currentTarget.reset()
 }
 
-function onSearchMoreBtn(){
-    newsApiServise.fetchArticels()
+function onLoadhMoreBtn(){
+    newsApiServise.fetchArticels().then(renderArticles)
+}
+
+function renderArticles(articles){
+    refs.articles.insertAdjacentHTML('beforeend', articlesTmpl(articles));
+
+}
+
+function clearArticlesContainer(){
+    refs.articles.innerHTML = '';
 }
