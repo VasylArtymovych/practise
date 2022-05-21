d17162f8cc5a52273cb4e7d9f1221c6be0d45de1
VasylArtymovych <art777vasss@gmail.com>
2022-05-21 22:08:26 +0300

    update video-player

--- src/JS/theory/test-theory.js
+++ src/JS/theory/test-theory.js
@@ -7,7 +7,7 @@ function inBetween(a, b){
         return el >=a && el <= b;
     }
 }
-console.log(array.filter(inBetween(5, 7)));
+// console.log(array.filter(inBetween(5, 7)));
 
 function inArray(arr){
     
@@ -15,7 +15,7 @@ function inArray(arr){
         return arr.includes(el);
     }
 }
-console.log(array.filter(inArray([1,3,7])));
+// console.log(array.filter(inArray([1,3,7])));
 
 // Method sort: 
 let users = [
@@ -29,6 +29,34 @@ function byField(fieldName){
 }
 
 //За ім’ям (Енн, Іван, Петро)
-console.log(users.sort(byField('name')));
+// console.log(users.sort(byField('name')));
 // За віком (Петро, Енн, Іван)
 // console.log(users.sort(byField('age')));
+
+
+
+//<<<<<<<<<<  Task >>>>>>>>>>>>>>
+//Your task is to write a function which returns the sum of following series upto nth term(parameter).
+// Series: 1 + 1/4 + 1/7 + 1/10 + 1/13 + 1/16 +...
+
+// function SeriesSum(n) {
+//     let sum = 0;
+//     for (let i = 0; i < n; i += 1){
+//         sum += 1/(1+i*3)
+//     }
+//     return sum.toFixed(2);
+// }
+
+
+// function SeriesSum(n) {
+//     let sum = 0;
+//     for (let i = 1; i < n * 3; i += 3){
+//         sum += 1/i
+//     }
+//     return sum.toFixed(2);
+// }
+
+
+// console.log(SeriesSum(5));
+
+
--- src/custom-video-player/script.js
+++ src/custom-video-player/script.js
@@ -4,43 +4,55 @@ const video = player.querySelector('.viewer');
 const skipBtn = player.querySelectorAll('[data-skip]');
 const range = player.querySelectorAll('.player__slider');
 const progressBar = player.querySelector('.progress__filled');
+const progress = player.querySelector('.progress');
 
+let mouseDown = false;
 
-toggle.addEventListener('click', togglePlay)
-video.addEventListener('click', togglePlay)
-video.addEventListener('play', updateBtn)
-video.addEventListener('pause', updateBtn)
-video.addEventListener('timeupdate', hundleProgress)
+toggle.addEventListener('click', togglePlay);
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateBtn);
+video.addEventListener('pause', updateBtn);
+video.addEventListener('timeupdate', hundleProgress);
+progress.addEventListener('click', rewind);
+progress.addEventListener('mousedown', ()=>(mouseDown = true));
+progress.addEventListener('mouseup', ()=>(mouseDown = false));
+progress.addEventListener('mousemove', () => { mouseDown && rewind(event) });
 
 function togglePlay () {
 	const method = video.paused ? 'play' : 'pause';
 	video[method]();
-}
+};
 
 function updateBtn() {
 	const icon = video.paused ? '▶️' : '❚ ❚';
 	toggle.textContent = icon;
-}
-
+};
+// add eventListener on btn's btn to skip video 10s / 25s
 skipBtn.forEach((btn) => {
 	btn.addEventListener('click', skip);
-})
+});
 
 function skip(event) {
-	console.log(this.dataset.skip);
-	video.currentTime += parseFloat(this.dataset.skip);
+	video.currentTime += parseFloat(event.currentTarget.dataset.skip);
 };
-
+//add eventListener on two range's to change (volume / speed)
 range.forEach((input) => {
 	input.addEventListener('input', hundleChangeUpdate);
 });
 
 function hundleChangeUpdate(event) {
-	video[this.name] = this.value;
+	const name = event.currentTarget.name;
+	const value = event.currentTarget.value;
+	video[name] = value;
+	// video[this.name] = this.value;
+};
+// display progress (yelow line on duration);
+function hundleProgress() {
+	const percent = video.currentTime / video.duration * 100;
+	progressBar.style.flexBasis = `${percent}%`
 };
 
-function hundleProgress(params) {
-	const parcent = video.currentTime / video.duration * 100;
-	progressBar.style.flexBasis = `${parcent}%`
-}
-
+function rewind(event) {
+	const rewindTime = (event.offsetX / progress.offsetWidth) * video.duration;
+	video.currentTime = rewindTime;
+};
