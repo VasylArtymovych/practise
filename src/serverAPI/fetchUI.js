424ad08efabf56d75b422d428df64cfa8518c17d
VasylArtymovych <art777vasss@gmail.com>
2022-06-07 10:29:55 +0300

    add async to server API

--- src/serverAPI/fetchUI.js
+++ src/serverAPI/fetchUI.js
@@ -9,36 +9,40 @@ const refs = {
 
 let items = [];
 
+///<<<<< async - await >>>>>>>>
+
 refs.fetchBtn.addEventListener('click', onFetchBtn);
 
-function onFetchBtn(){
-    showLoader()
-    getItems().then(data => {
+async function onFetchBtn(){
+    try {
+        showLoader();
+        const data = await getItems();
         items = data;
         renderItems();
-    })
-    .catch(err => console.log)
-    .finally(()=>{
         hideLoader();
         refs.fetchBtn.disabled=true;
-    })
+    } catch (error) {
+        onsole.log(error)
+    }
 };
 
 refs.form.addEventListener('submit', submitHandler);
 
-function submitHandler(evt){
-    evt.preventDefault();
-
-    let text = evt.currentTarget.elements.text.value;
-
-    const newItem = {
+async function submitHandler(evt){
+    try {
+        evt.preventDefault();
+        let text = evt.currentTarget.elements.text.value;
+        const newItem = {
         text,
         isDone: false,
-    };
-    createItem(newItem)
-    .then(()=>{
+        };
+        const data = await createItem(newItem);
         refs.form.reset();
-    });
+    }
+    catch (error) {
+        console.log(error.message);
+    }
+    
 }
 
 refs.ul.addEventListener('click', onItemClick);
@@ -61,36 +65,34 @@ function onItemClick(evt){
 };
 
 // create new item:
-function createItem(newItem){
-    showLoader();
-    return postItem(newItem)
-    .then(data => {items.push(data)})
-    .then(()=>{
+async function createItem(newItem){
+    try {
+        showLoader();
+        const createdItem = await postItem(newItem);
+        items.push(createdItem);
         renderItems();    
-    })
-    .catch(error => {
+        hideLoader();
+        return createdItem;
+    } catch (error) {
         console.log(error.message);
-    })
-    .finally(hideLoader);
+    }
+    
 };
 
 // change checkbox:
-function toggleItem(id){
-    showLoader();
-    const item = items.find(item => item.id === id);
-    console.log(item);
-
-    updateItem(id, {isDone: !item.isDone})
-    .then(()=>{
+async function toggleItem(id){
+    try {
+        showLoader();
+        const item = items.find(item => item.id === id);
+        const response = await updateItem(id, {isDone: !item.isDone})
         items = items.map(item => item.id === id ? {...item, isDone: !item.isDone} : item);
-    })
-    .then(()=> {
         renderItems();
-    })
-    .finally(()=>{
         hideLoader()
-    })
-
+        return response.data;
+    } catch (error) {
+        console.log(error);
+    }
+    
 };
 
 // delete item from list:
@@ -129,3 +131,128 @@ function showLoader(){
 function hideLoader(){
     refs.loader.classList.remove('show');
 };
+
+
+
+// use promise - fetch /////
+
+// refs.fetchBtn.addEventListener('click', onFetchBtn);
+
+// function onFetchBtn(){
+//     showLoader()
+//     getItems().then(data => {
+//         items = data;
+//         renderItems();
+//     })
+//     .catch(err => console.log)
+//     .finally(()=>{
+//         hideLoader();
+//         refs.fetchBtn.disabled=true;
+//     })
+// };
+
+// refs.form.addEventListener('submit', submitHandler);
+
+// function submitHandler(evt){
+//     evt.preventDefault();
+
+//     let text = evt.currentTarget.elements.text.value;
+
+//     const newItem = {
+//         text,
+//         isDone: false,
+//     };
+//     createItem(newItem)
+//     .then(()=>{
+//         refs.form.reset();
+//     });
+// }
+
+// refs.ul.addEventListener('click', onItemClick);
+
+// function onItemClick(evt){
+//     if (evt.target === evt.currentTarget) return 
+//     const parent = evt.target.closest('li');
+//     const {id} = parent.dataset;
+
+//     switch (evt.target.nodeName){
+//         case 'BUTTON':
+//             removeItem(id);
+//             break;
+//         case 'INPUT':
+//             toggleItem(id);
+//             break;
+//         default:
+//             break;
+//     };
+// };
+
+// // create new item:
+// function createItem(newItem){
+//     showLoader();
+//     return postItem(newItem)
+//     .then(data => {items.push(data)})
+//     .then(()=>{
+//         renderItems();    
+//     })
+//     .catch(error => {
+//         console.log(error.message);
+//     })
+//     .finally(hideLoader);
+// };
+
+// // change checkbox:
+// function toggleItem(id){
+//     showLoader();
+//     const item = items.find(item => item.id === id);
+//     console.log(item);
+
+//     updateItem(id, {isDone: !item.isDone})
+//     .then(()=>{
+//         items = items.map(item => item.id === id ? {...item, isDone: !item.isDone} : item);
+//     })
+//     .then(()=> {
+//         renderItems();
+//     })
+//     .finally(()=>{
+//         hideLoader()
+//     })
+
+// };
+
+// // delete item from list:
+// function removeItem(id){
+//     showLoader();
+//     deleteItem(id)
+//     .then(()=> {
+//         items = items.filter(item => item.id !== id);
+//         renderItems();
+//     })
+//     .finally(()=>{
+//         hideLoader();
+//     })
+//     .catch(err => {console.log(err.message)});
+// }
+
+// function renderItems(){
+//     const list = items.map(template).join('');
+//     refs.ul.innerHTML='';
+//     refs.ul.insertAdjacentHTML('beforeend', list);
+// };
+
+// function template({id, text, isDone}){
+//     return `<li data-id="${id}">
+//                 <label>
+//                     <input type="checkbox" ${isDone ? 'checked' : ''} />
+//                     <apan>${text}</span>
+//                 </label>
+//                 <button>x</button>
+//             </li>`;
+// };
+
+// function showLoader(){
+//     refs.loader.classList.add('show');
+// };
+// function hideLoader(){
+//     refs.loader.classList.remove('show');
+// };
