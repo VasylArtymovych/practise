91c6dea547ab8fc1e8578c0c485a6787fe7cab2c
VasylArtymovych <art777vasss@gmail.com>
2022-06-12 01:03:23 +0300

    add pagination

--- src/Filmoteka/index.js
+++ src/Filmoteka/index.js
@@ -1,72 +1,176 @@
 const api_key = "9c009c00402fb866baf05bd346a2d01d";
 const img_url = 'https://image.tmdb.org/t/p/w500';
 const BASE_URL = "https://api.themoviedb.org/3/";
-const trending = "https://api.themoviedb.org/3/trending/movie/day?";
-
-const current_page = 1;
-const arrOfBtn = [];
-
-// console.log(arrOfBtn);
+const trending = "https://api.themoviedb.org/3/trending/movie/day";
+const movie_genres = `https://api.themoviedb.org/3/discover/movie`;
+const SEARCH_URL = `https://api.themoviedb.org/3/search/movie?api_key=${api_key}`;
 
 
-
-const movies_list = document.querySelector(".movies_list");
-const btn_list = document.querySelector('.btn_list');
-
-
-async function fetchData(){
+// class FetchData {
     
-    const response = await fetch(trending + new URLSearchParams({
-        api_key: api_key,
-        page: current_page
-    }));
-    console.log(response);
+//     constructor(){
+//         this.page = 1;
+//     }
+
+//     async moviesFetch(){
+//         const response = await fetch(trending + new URLSearchParams({
+//             api_key: api_key,
+//             page: this.page,
+//         }))
+//         return response.json();
+//     }
+
+//     setpage(newPage){
+//         this.page = newPage;
+//     }
+
+// }
+
+// const api = new FetchData();
+
+// const response =  api.moviesFetch();
+// response.then(data => {
+//     console.log(data);
+
+//     createCardList(data.results)
     
-    return response.json();
+// })
+
+// const createCardList = (cards) => {
+//     console.log(cards);
+//     const list = cards.map(createCard).join('');
+//     card_list.insertAdjacentHTML('beforeend', list);
+    
+// }
+
+const createCard = (data) => {
+    // {backdrop_path, poster_path, genre_ids, release_date, title, id}
+    // if (backdrop_path === null){
+    //     backdrop_path = poster_path;
+    //     if(poster_path === null) return;
+    // }
+    return data.map(({backdrop_path, poster_path, genre_ids, release_date, title, id}) =>{
+    
+    return `
+    <li class="card_item" id="${id}>
+        <div class="img_thumb">
+            <img src="${img_url}${backdrop_path}" alt="${title}"/>
+        </div>
+        <p class="title">${title}</p>
+        <p class="genre">${genre_ids.map(genre => {
+            return `<span>${genre}</span>`;
+        })}</p>
+        <p class="year">${release_date.slice(0, 4)}</p>
+    </li>
+    `
+})
 }
-const res = fetchData('popular');
 
-res.then(data => {
-    console.log(data);
-    
-    template(data.results);
-    // createBtns(data.total_pages);
-    displayBtn(data.total_pages, btn_list, 10, current_page);
-});
+// const buttons = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22'];
+// const per_page = 5;
+// const page = 1; 
 
-// function createBtns(total, ){
-//     for (let i = 1; i <= total; i += 1){
-//     arrOfBtn.push(i)
+// function createBtnLIst(buttons,  per_page, page){
+//    let start = per_page * (page -1);
+//     let end =  start + per_page;
+//     const render_btns = buttons.slice(start, end);
+//     for (let i = 1; i < render_btns.length+1; i += 1){
+//         const btn = `<button type="button" data-action="${i}">${i}</button>`
+//         btn_container.insertAdjacentHTML('beforeend', btn);
 //     }
 // }
 
-function displayBtn(total, wraper, per_page, page){
-    wraper.innerHTML = '';
+// createBtnLIst(buttons, per_page, page);
 
-    const start = page;
-    const end = start + per_page;
+// btn_container.addEventListener('click', onBtnClick)
 
-    for (let i = start; i < end; i += 1){
-        const btn = document.createElement('button');
-        btn.type = "button";
-        btn.textContent = i;
-        wraper.append(btn);
+// function onBtnClick(event){
+//     if (event.target === event.currentTarger) return;
+//     const page = event.target.dataset.action;
+//     api.setpage(page);
+//     api.moviesFetch().then(data => {
+//         card_list.innerHTML = '';
+//         createCardList(data.results);
+//     })
+// }
 
-    }
-    console.log('hello');
+const card_list = document.querySelector('.card_list');
+const btn_container = document.querySelector('.btn_container');
+
+let globalPage;
+
+function searchTrend(page=1){
+    card_list.innerHTML = '';
+
+    fetch(trending + "?api_key=" + api_key + "&page=" + page).then(response => response.json()).then(data => {
+        console.log(data);
+        const markup =  createCard(data.results);
+        
+        card_list.insertAdjacentHTML('beforeend', markup);
+        createPagination(data.page, data.total_pages);
+
+    })
 }
+searchTrend();
 
-
-
-
-function template(arr){
-
-    const temp = arr.map(({poster_path, title}) => `<div>
-    <img src="${img_url}${poster_path}" alt="${title}" width="300">
-    <p>${title}</p>
-    <p>${"hello"}</p>
-    </div>`).join('');
-    movies_list.insertAdjacentHTML('beforeend', temp);
+function createPagination(page, totalPage){
+    let markup = '';
+    const beforTwoPage = page - 2;
+    const beforePage = page -1;
+    const afterPage = page + 1; 
+    const afterTwoPage = page + 2;
+    globalPage = page;
     
+    if (page > 1){
+        markup += '<li>&laquo;</li>';
+    }
+    if(page > 1){
+        markup += '<li>1</li>';
+        if(page > 4){
+            markup += '...';
+        }
+    }
+    if(page > 3){
+        markup += `<li>${beforTwoPage}</li>`
+    }
+    if(page > 2){
+        markup += `<li>${beforePage}</li>`
+    }
+    markup += `<li class='active'>${page}</li>`
+    if(page < totalPage - 1){
+        markup += `<li>${afterPage}</li>`
+    }
+    if(page < totalPage - 2){
+        markup += `<li>${afterTwoPage}</li>`
+    }
+    if(page < totalPage - 3){
+        markup += `<li>...</li>`
+    }
+    if(page < totalPage){
+        markup += `<li>${totalPage}</li>`
+        markup += '<li>&raquo;</li>'
+    }
+    btn_container.innerHTML = markup;
+    btn_container.addEventListener('click', onBtnClick);
 }
 
+function onBtnClick(event){
+    if(event.target.nodeName !== "LI"){
+        return;
+    
+    }
+    const target = event.target.textContent;
+    switch (target){
+        case '«': 
+            searchTrend(globalPage - 1);
+        break;
+        case '...': 
+        break;
+        case '»': 
+        searchTrend(globalPage + 1);
+        break;
+        default: 
+        searchTrend(target);
+    }
+    console.dir(event.target.textContent);
+}
